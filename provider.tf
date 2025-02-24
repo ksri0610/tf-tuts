29070225841467b474965354c6bf6e26ebe92ddc
ksri0610 <ksri0610@gmail.com>
2025-02-24 14:31:08 +0530

    added somecontent

--- provider.tf
+++ provider.tf
@@ -1,15 +1,15 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 5.0.0"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0.0"
+    }
+  }
 
- backend "s3" {
-   bucket = "tf-tuts-state"
-   key    = "state"
-   region = "eu-central-1"
- }
+  backend "s3" {
+    bucket = "tf-tuts-state1"
+    key    = "mumbai"
+    region = "ap-south-1"
+  }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "ami" {
-   type        = string
-   description = "Ubuntu AMI ID in N. Virginia Region"
-   default     = "ami-065deacbcaac64cf2"
+  type        = string
+  description = "Ubuntu AMI ID in N. Virginia Region"
+  default     = "ami-03b8adbf322415fd0"
 }
 
 variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t2.micro"
+  type        = string
+  description = "Instance type"
+  default     = "t2.micro"
 }
 
 variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
+  type        = string
+  description = "Name of the EC2 instance"
+  default     = "My EC2 Instance"
 }
 
